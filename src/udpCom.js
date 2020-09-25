e52db07cc60843ab8e8a3f89c208f1e8bcfb81d0
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-09-25 19:00:28 +0800

    Added the node js server code.

--- src/udpCom.js
+++ src/udpCom.js
@@ -3,6 +3,7 @@ var PORT = 5005;
 var HOST = '127.0.0.1';
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
+
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
