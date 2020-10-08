a407a713b242fc4cbd5639ea42ef943126fd0518
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-10-08 22:47:00 +0800

    encapsulate server function and added the test case.

--- src/udpCom.js
+++ src/udpCom.js
@@ -1,4 +1,33 @@
 //sever ------------------------------------------------------
+var dgram = require('dgram');
+
+exports.udpServer = function (udpHost, udpPort, msgFunc) {
+    var server = dgram.createSocket('udp4');
+    
+    server.on('error', (err) => {
+        console.log(`server error:\n${err.stack}`);
+        server.close();
+      });
+
+    // pass in callbacks
+    server.on('listening', function () {
+        var address = server.address();
+        console.log('UDP Server listening on ' + address.address + ":" + address.port);
+    });
+    server.on('message', function (message, remote) {
+        console.log(remote.address + ':' + remote.port + ' - ' + message);
+        var msgResponse = msgFunc(message);
+        server.send(msgResponse, 0, msgResponse.length, remote.port, remote.address, function (err, bytes) {
+            if (err) throw err;
+            console.log('UDP server message sent to ' + remote.address + ':' + remote.port);
+        });
+    });
+    server.bind(udpHost, udpPort);
+  };
+
+
+
+
 var PORT = 5005;
 var HOST = '127.0.0.1';
 var dgram = require('dgram');
---
+++ src/udpComTest.js
@@ -0,0 +1,5 @@
+var dt = require('./udpCom');
+
+
+dt.udpServer('127.0.0.1', 5005);
+
