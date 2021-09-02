c828e05a96b8b78958102ea4dfe64916c246ec74
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2021-09-02 22:58:07 +0800

    Added the name and threat count query.

--- src/pages/gateway-api-graphql/threatEvents/resolvers/nationalactor.js
+++ src/pages/gateway-api-graphql/threatEvents/resolvers/nationalactor.js
@@ -12,6 +12,56 @@ const moment = require('moment');
 
 module.exports = {
   Query: {
+
+    threatHourCounts: () => {
+      let feedback = '' ;
+      let query = {
+        "queryType": "timeseries",
+        "dataSource": {
+          "type": "table",
+          "name": "ds-suspected-ip-2019"
+        },
+        "intervals": {
+          "type": "intervals",
+          "intervals": [
+            "-146136543-09-08T08:23:32.096Z/146140482-04-24T15:36:27.903Z"
+          ]
+        },
+        "descending": false,
+        "virtualColumns": [],
+        "filter": null,
+        "granularity": "HOUR",
+        "aggregations": [
+          {
+            "type": "count",
+            "name": "a0"
+          }
+        ],
+        "postAggregations": [],
+        "limit": 2147483647,
+        "context": {
+          "skipEmptyBuckets": true,
+          "sqlQueryId": "2324b14f-0397-4697-859c-9aeffec7c32b",
+          "timestampResultField": "d0"
+        }
+      };
+
+      return new Promise((resolve, reject) => {
+        druid.query.post('/', query).then(res => {
+          let msg = [];
+          for (let obj of res.data){
+            msg.push(obj['result']);
+          }
+            feedback = JSON.stringify(msg, '');
+          resolve(feedback);
+        }).catch(err => {
+          console.error(err)
+          reject(err)
+        })
+      });
+      return feedback;
+    },
+
     threatActor: () => {
       let feedback = '123';
       let query = {
@@ -80,5 +130,29 @@ module.exports = {
 
       //return 'Test Success, GraphQL server is up & running !!' 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 }
--- src/pages/gateway-api-graphql/threatEvents/resolvers/nationalname.js
+++ src/pages/gateway-api-graphql/threatEvents/resolvers/nationalname.js
@@ -54,6 +54,66 @@ module.exports = {
 
             return new Promise((resolve, reject) => {
                 druid.query.post('/', query).then(res => {
+                  let msg = []
+                  for (let obj of res.data){
+                    msg.push(obj['result']);
+                  }
+                    feedback = JSON.stringify(msg, '');
+                    resolve(feedback);
+                }).catch(err => {
+                    console.error(err)
+                    reject(err)
+                })
+            });
+            //return 'Test Success, GraphQL server is up & running !!' 
+        },
+
+        threatSector: () => {
+            let feedback = '123';
+            let query = {
+                "queryType": "topN",
+                "dataSource": {
+                  "type": "table",
+                  "name": "ds-suspected-ip-2019"
+                },
+                "virtualColumns": [],
+                "dimension": {
+                  "type": "default",
+                  "dimension": "srcSector",
+                  "outputName": "d0",
+                  "outputType": "STRING"
+                },
+                "metric": {
+                  "type": "numeric",
+                  "metric": "a0"
+                },
+                "threshold": 100,
+                "intervals": {
+                  "type": "intervals",
+                  "intervals": [
+                    "-146136543-09-08T08:23:32.096Z/146140482-04-24T15:36:27.903Z"
+                  ]
+                },
+                "filter": null,
+                "granularity": {
+                  "type": "all"
+                },
+                "aggregations": [
+                  {
+                    "type": "count",
+                    "name": "a0"
+                  }
+                ],
+                "postAggregations": [],
+                "context": {
+                  "sqlOuterLimit": 100,
+                  "sqlQueryId": "71970e7d-0820-4afd-b3aa-575e08a5b0d6"
+                },
+                "descending": false
+              };
+
+            return new Promise((resolve, reject) => {
+                druid.query.post('/', query).then(res => {
                     feedback = JSON.stringify(res.data, '');
                     console.log('p1', feedback);
                     //resolve(res.data);
@@ -64,6 +124,73 @@ module.exports = {
                 })
             });
             //return 'Test Success, GraphQL server is up & running !!' 
-        }
+        },
+
+        threatClient: (ClientName) => {
+            let feedback = '123';
+            //let nameString = '"'+ClientName+'"';
+            let query = {
+                "queryType": "topN",
+                "dataSource": {
+                  "type": "table",
+                  "name": "ds-suspected-ip-2019"
+                },
+                "virtualColumns": [],
+                "dimension": {
+                  "type": "default",
+                  "dimension": "__time",
+                  "outputName": "d0",
+                  "outputType": "LONG"
+                },
+                "metric": {
+                  "type": "dimension",
+                  "previousStop": null,
+                  "ordering": {
+                    "type": "numeric"
+                  }
+                },
+                "threshold": 100,
+                "intervals": {
+                  "type": "intervals",
+                  "intervals": [
+                    "-146136543-09-08T08:23:32.096Z/146140482-04-24T15:36:27.903Z"
+                  ]
+                },
+                "filter": {
+                  "type": "selector",
+                  "dimension": "srcSector",
+                  "value": ClientName,
+                  "extractionFn": null
+                },
+                "granularity": {
+                  "type": "all"
+                },
+                "aggregations": [
+                  {
+                    "type": "count",
+                    "name": "a0"
+                  }
+                ],
+                "postAggregations": [],
+                "context": {
+                  "sqlOuterLimit": 100,
+                  "sqlQueryId": "44dca133-f753-4468-a5ff-8e8d772dacde"
+                },
+                "descending": false
+              };
+
+            return new Promise((resolve, reject) => {
+                druid.query.post('/', query).then(res => {
+                    feedback = JSON.stringify(res.data, '');
+                    console.log('p1', feedback);
+                    //resolve(res.data);
+                    resolve(feedback);
+                }).catch(err => {
+                    console.error(err)
+                    reject(err)
+                })
+            });
+            //return 'Test Success, GraphQL server is up & running !!' 
+        },
     }
 }
