f41f90653790756bc026ac39ba4059e36ccbb296
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2021-09-03 15:27:45 +0800

    Fixed the problem to pass parameters to query string.

--- src/pages/gateway-api-graphql/threatEvents/resolvers/nationalname.js
+++ src/pages/gateway-api-graphql/threatEvents/resolvers/nationalname.js
@@ -126,71 +126,62 @@ module.exports = {
             //return 'Test Success, GraphQL server is up & running !!' 
         },
 
-        threatClient: (ClientName) => {
-            let feedback = '123';
-            //let nameString = '"'+ClientName+'"';
-            let query = {
-                "queryType": "topN",
-                "dataSource": {
-                  "type": "table",
-                  "name": "ds-suspected-ip-2019"
-                },
-                "virtualColumns": [],
-                "dimension": {
-                  "type": "default",
-                  "dimension": "__time",
-                  "outputName": "d0",
-                  "outputType": "LONG"
-                },
-                "metric": {
-                  "type": "dimension",
-                  "previousStop": null,
-                  "ordering": {
-                    "type": "numeric"
-                  }
-                },
-                "threshold": 100,
-                "intervals": {
-                  "type": "intervals",
-                  "intervals": [
-                    "-146136543-09-08T08:23:32.096Z/146140482-04-24T15:36:27.903Z"
-                  ]
-                },
-                "filter": {
-                  "type": "selector",
-                  "dimension": "srcSector",
-                  "value": ClientName,
-                  "extractionFn": null
-                },
-                "granularity": {
-                  "type": "all"
-                },
-                "aggregations": [
-                  {
-                    "type": "count",
-                    "name": "a0"
-                  }
-                ],
-                "postAggregations": [],
-                "context": {
-                  "sqlOuterLimit": 100,
-                  "sqlQueryId": "44dca133-f753-4468-a5ff-8e8d772dacde"
-                },
-                "descending": false
-              };
-
-            return new Promise((resolve, reject) => {
-                druid.query.post('/', query).then(res => {
-                    feedback = JSON.stringify(res.data, '');
-                    console.log('p1', feedback);
-                    //resolve(res.data);
-                    resolve(feedback);
-                }).catch(err => {
-                    console.error(err)
-                    reject(err)
-                })
-            });
-            //return 'Test Success, GraphQL server is up & running !!' 
+    threatClient: (root, { ClientName }, { user }) => {
+      let feedback = '123';
+      //let nameString = '"'+ClientName+'"';
+      console.log('>>>threat Client', ClientName);
+      let query = {
+        "queryType": "timeseries",
+        "dataSource": {
+          "type": "table",
+          "name": "ds-suspected-ip-2019"
         },
-    }
+        "intervals": {
+          "type": "intervals",
+          "intervals": [
+            "-146136543-09-08T08:23:32.096Z/146140482-04-24T15:36:27.903Z"
+          ]
+        },
+        "descending": false,
+        "virtualColumns": [],
+        "filter": {
+          "type": "selector",
+          "dimension": "srcSector",
+          "value": ClientName,
+          "extractionFn": null
+        },
+        "granularity": "HOUR",
+        "aggregations": [
+          {
+            "type": "count",
+            "name": "a0"
+          }
+        ],
+        "postAggregations": [],
+        "limit": 100,
+        "context": {
+          "skipEmptyBuckets": true,
+          "sqlOuterLimit": 100,
+          "sqlQueryId": "c8e13274-06c7-4c9d-84f8-af30436dddc4",
+          "timestampResultField": "d0"
+        }
+      };
+
+      return new Promise((resolve, reject) => {
+        druid.query.post('/', query).then(res => {
+          let msg = [];
+          for (let obj of res.data) {
+            msg.push(obj['result']);
+          }
+          feedback = JSON.stringify(msg, '');
+          console.log('p1', feedback);
+          resolve(feedback);
+        }).catch(err => {
+          console.error(err)
+          reject(err)
+        })
+      });
+      //return 'Test Success, GraphQL server is up & running !!' 
+    },
+  }
 }
